8dd16f6c82abdd238028c4247d84a4cd2b2f5cab
YohnWang <347220312@qq.com>
2017-03-02 21:39:03 +0800

    find a bug in tree_del and fix it

--- tree/binary-search-tree.c
+++ tree/binary-search-tree.c
@@ -9,29 +9,31 @@ struct tree
     tree *right;
 };
 
+//the primary operations of binary tree
+
 void tree_init(tree *root)
 {
-    root->parent=root;
+    root->parent=NULL;
     root->left=NULL;
     root->right=NULL;
 }
 
-inline tree* tree_left(tree *p)
+static inline tree* tree_left(tree *p)
 {
     return p->left;
 }
-inline tree* tree_right(tree *p)
+static inline tree* tree_right(tree *p)
 {
     return p->right;
 }
-inline tree* tree_parent(tree *p)
+static inline tree* tree_parent(tree *p)
 {
     return p->parent;
 }
 
 tree* tree_root(tree *p)
 {
-    if(tree_parent(p)!=p)
+    if(tree_parent(p)!=NULL)
         return tree_root(tree_parent(p));
     else
         return p;
@@ -45,6 +47,90 @@ void tree_insert(tree *root,tree *left,tree *right)
         left->parent=root;
     if(right!=NULL)
         right->parent=root;
+}
+
+tree* tree_most_left(tree *root)
+{
+    if(tree_left(root)!=NULL)
+        return tree_most_left(tree_left(root));
+    else
+        return root;
+}
+tree* tree_most_right(tree *root)
+{
+    if(tree_right(root)!=NULL)
+        return tree_most_right(tree_right(root));
+    else
+        return root;
+}
+
+void tree_del(tree *node)
+{
+    if(tree_left(node)==NULL && tree_right(node)==NULL)//the node without any children
+    {
+        tree *parent=tree_parent(node);
+        if(parent==NULL)//the node is the root
+            return;
+        if(parent->left==node)//is left or right child
+            parent->left=NULL;
+        else
+            parent->right=NULL;
+        tree_init(node);//remove any information about tree in this node
+    }
+    else if(tree_left(node)==NULL && tree_right(node)!=NULL)//the node have a right tree
+    {
+        tree *parent=tree_parent(node);
+        tree *right=tree_right(node);
+        if(parent==NULL)//the node is the root
+        {
+            right->parent=NULL;
+            tree_init(node);
+            return;
+        }
+        if(parent->left==node)
+            parent->left=right;
+        else
+            parent->right=right;
+        right->parent=parent;
+        tree_init(node);
+    }
+    else if(tree_left(node)!=NULL && tree_right(node)==NULL)//the node have a left tree
+    {
+        tree *parent=tree_parent(node);
+        tree *left=tree_left(node);
+        if(parent==NULL)//the node is the root
+        {
+            left->parent=NULL;//take the left tree as the root
+            tree_init(node);
+            return;
+        }
+        if(parent->left==node)//is left or right child
+            parent->left=tree_left(node);
+        else
+            parent->right=tree_left(node);
+        left->parent=parent;
+        tree_init(node);
+    }
+    else //the node with two children
+    {
+        tree *left=tree_left(node),*right=tree_right(node);
+        tree *most_right=tree_most_right(left);//used to replace the node
+        tree_del(most_right);//delete the node in the tree
+        most_right->left=left;
+        most_right->right=right;
+        tree *parent=tree_parent(node);
+        if(parent==NULL)
+            most_right->parent=NULL;
+        else
+        {
+            most_right->parent=parent;
+            if(tree_left(parent)==node)
+                parent->left=most_right;
+            else
+                parent->right=most_right;
+        }
+        tree_init(node);
+    }
 }
 
 void tree_prev_visit(tree *root,void (*handler)(tree *root,void *args),void *args)
@@ -82,7 +168,7 @@ void tree_post_visit(tree *root,void (*handler)(tree *root,void *args),void *arg
 
 }
 
-
+//the operations of binary search tree
 
 void tree_add(tree *root,int (*cmp)(tree *a,tree*b),tree *new)
 {
@@ -100,51 +186,6 @@ void tree_add(tree *root,int (*cmp)(tree *a,tree*b),tree *new)
         else
             tree_insert(root,tree_left(root),new);
     }
-}
-
-void tree_del(tree *node)
-{
-    if(tree_left(node)==NULL && tree_right(node)==NULL)
-    {
-        tree *parent=node->parent;
-        if(parent->left==node)
-            parent->left=NULL;
-        else
-            parent->right=NULL;
-    }
-    else if(tree_left(node)==NULL && tree_right(node)!=NULL)
-    {
-        tree *parent=node->parent;
-        if(parent->left==node)
-            parent->left=tree_right(node);
-        else
-            parent->right=tree_right(node);
-    }
-    else if(tree_left(node)!=NULL && tree_right(node)==NULL)
-    {
-        tree *parent=node->parent;
-        if(parent->left==node)
-            parent->left=tree_left(node);
-        else
-            parent->right=tree_left(node);
-    }
-    else
-    {
-        tree *left=tree_left(node);
-        while(tree_right(left))
-        {
-            left=tree_right(left);
-        }
-        tree_del(left);
-
-        left->left=node->left;
-        left->right=node->right;
-        if(tree_parent(node)!=node)
-            left->parent=node->parent;
-        else
-            left->parent=left;
-    }
-
 }
 
 struct number
@@ -168,7 +209,7 @@ int number_cmp(tree *a,tree *b)
 int main(int argc,char *argv[])
 {
     struct number a[10];
-    int b[]={9,1,3,4,7,5,2,6,8,10};
+    int b[]={49,1,3,74,7,5,2,6,18,10};
     for(int i=0;i<sizeof a/sizeof a[0];i++)
     {
         tree_init(&a[i].tree);
@@ -178,7 +219,16 @@ int main(int argc,char *argv[])
     {
         tree_add(&a[0].tree,number_cmp,&a[i].tree);
     }
+    printf("%d\n",((struct number*)tree_most_right(&a[0].tree))->x);
+    tree_del(&a[1].tree);
+    tree_del(&a[5].tree);
+    tree_del(&a[9].tree);
     tree_del(&a[0].tree);
-    tree_inorder_visit(tree_root(&a[8].tree),print,NULL);
+    tree_del(&a[7].tree);
+    //for(int i=1;i<sizeof a/sizeof a[0];i++)
+    //{
+    //    tree_del(&a[i].tree);
+    //}
+    tree_inorder_visit((&a[8].tree),print,NULL);
     printf("\n%d",((struct number*)tree_root(&a[8].tree))->x);
 }
